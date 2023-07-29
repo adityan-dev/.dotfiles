a48ff9b0f4fbc3ee4d71da4a2e4d29a57a7ca091
adityan <official.adityan@gmail.com>
2023-07-29 17:49:01 +0530

    thor-auto-commit

--- .config/nix/nixos/data-drive.nix
+++ .config/nix/nixos/data-drive.nix
@@ -1,8 +1,16 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
-  fileSystems."/media/data" =
-    { device = "/dev/disk/by-uuid/e0f28002-546e-4921-b366-01db4b7fd78d";
-      fsType = "ext4";
-      options = [ "nofail" ];
-    };
+  # systemd.user.services.<your-service-name> = {
+  #   description = "...";
+  #   serviceConfig.PassEnvironment = "DISPLAY";
+  #   script = ''
+ 
+  #   '';
+  #   wantedBy = [ "multi-user.target" ]; # starts after login
+  # };
+
+  # fileSystems."/media/data" =
+  #   { device = "/dev/disk/by-uuid/e0f28002-546e-4921-b366-01db4b7fd78d";
+  #     fsType = "ext4";
+  #   };
 }
